788d4a2f23d121e24eeb13389ed9eabe16217789
Danilo Aversa <danilo.aversa@gmail.com>
2026-06-05 15:51:59 +0200

    v6.9.2a • 2026-06-05 15:51
    Changed files: 3
    Change scale: minor
    
    - eatures/darken-location/composer/darken-location-composer.index.js
    - package.json
    - features/darken-location/composer/darken-location-composer.6-9-hotfix.css

--- features/darken-location/composer/darken-location-composer.index.js
+++ features/darken-location/composer/darken-location-composer.index.js
@@ -1,5 +1,6 @@
 import "./darken-location-composer.styles.css";
 import "./darken-location-composer.alignment.css";
+import "./darken-location-composer.6-9-hotfix.css";
 
 export { default } from "./DarkenLocationComposerPage.jsx";
 export { default as DarkenLocationComposerPage } from "./DarkenLocationComposerPage.jsx";
